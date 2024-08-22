dafdf15c559713760f560d89502641278f39e3c6
Ubuntu <jvalle0514@gmail.com>
2024-08-22 23:35:16 +0000

    add rule

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.0/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
 
     <title>Iniciar Sesión</title>
